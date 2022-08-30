b65794c3193d24d9ae8e6adb1906a52b933633d8
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-30 11:13:09 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,12 +5,11 @@ import storage from 'redux-persist/lib/storage';
 
 export const contacts = createSlice({
   name: 'contacts',
-  initialState: {
     items: [],
-  },
+ 
   reducers: {
     addNewContact(state, action) {
-      state.items.push(action.payload);
+      state.items = [...state.items, action.payload];
     },
     deleteContact(state, action) {
       state.items = state.items.filter(({ id }) => id !== action.payload);
